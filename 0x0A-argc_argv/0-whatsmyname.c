f07bce7e90f5c7d5971ef040c687254dfdfc7631
Mouhcine <mouhcine.akroud.40@gmail.com>
2023-04-10 19:31:17 -0700

    0x09. C - Static libraries

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - is function serves as the starting point for program execution.
+ * @argc:  is the number of arguments on the command line.
+ * @argv: is an array of arguments on the command line.
+ *
+ * Return: 0 always 
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
