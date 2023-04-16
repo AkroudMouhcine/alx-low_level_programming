4328a68e081dfd3e0607a0c1e37aaa79f35f3783
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-04-16 03:00:35 +0000

    I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that splits a string into words.
+ * @ac: var.
+ * @av: var.
+ * Return: NULL or pointer.
+ */
+
+char *argstostr(int ac, char **av)
