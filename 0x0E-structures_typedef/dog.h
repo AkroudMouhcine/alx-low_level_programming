539496378cede573d07cfbddbfc644362bd4485b
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-04-18 18:26:45 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct.
+ *
+ * @name: dog's name.
+ * @age: dog age.
+ * @owner: the dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
