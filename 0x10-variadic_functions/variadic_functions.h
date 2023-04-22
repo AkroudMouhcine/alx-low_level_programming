2a3b3e251495f36e9a42f77b2ddb665aef910d4a
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-04-22 02:59:19 +0000

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,11 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct print_function
+{
+	char *format;
+	void (*function)(va_list args);
+} print_function;
+
+
 #endif
