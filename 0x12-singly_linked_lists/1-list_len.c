315f8d04c0e1c2cc6af442b6f0e60e5fd28184b9
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-04-27 22:20:17 +0000

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ *
+ * @h: poiter.
+ *
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
